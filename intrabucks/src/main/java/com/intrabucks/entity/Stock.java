4e4517b7e188ca439dfec09c64a06390b7aa9647
jy <ji9626689@gmail.com>
2024-06-28 10:07:26 +0900

    빠져 있던 주석 추가

--- intrabucks/src/main/java/com/intrabucks/entity/Stock.java
+++ intrabucks/src/main/java/com/intrabucks/entity/Stock.java
@@ -13,7 +13,12 @@ import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+/**
+ * 재고 테이블(Stock) 엔티티로, 재고 테이블에 대한 내용을 담고 있음
+ * @author 구은재
+ * @version 1.0
+ * 2024-06-26
+ * **/
 @Entity
 @Data
 @Table(name = "Stock")
@@ -25,17 +30,17 @@ public class Stock {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "stock_seq")
 	@SequenceGenerator(name = "stock_seq", sequenceName = "stock_seq", allocationSize = 1)
 	@Column(name = "stock_id")
-	private Long stockId;
+	private Long stockId; // 재고 ID
 	
 	@ManyToOne
     @JoinColumn(name = "material_id", referencedColumnName = "material_id")
-    private Material material; 
+    private Material material; // 원자재 ID
 
     @Column(name = "stock_count")
-    private Integer stockCount; 
+    private Integer stockCount; // 재고수량
 
     @ManyToOne
     @JoinColumn(name = "store_id", referencedColumnName = "store_id")
-    private Store store; 
+    private Store store; // 매장 ID
 
 }
