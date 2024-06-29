7c74d5ad481e293de8ba063ac2ccce5a4790cf4e
YUBIN CHOE <cybin050300@naver.com>
2024-06-30 00:08:35 +0900

    Voice 인트라넷-공홈 서버 통신

--- intrabucks/src/main/java/com/intrabucks/voice/controller/VoiceController.java
+++ intrabucks/src/main/java/com/intrabucks/voice/controller/VoiceController.java
@@ -1,7 +1,21 @@
 package com.intrabucks.voice.controller;
 
+import com.intrabucks.voice.dto.voiceResponseDto;
+import com.intrabucks.voice.service.VoiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/customer")
 public class VoiceController {
+    @Autowired
+    private VoiceService voiceService;
+
+    @GetMapping("/voice")
+    public String voice(@RequestParam int voiceId) {
+        return voiceService.getVoice(voiceId);
+    }
 }
