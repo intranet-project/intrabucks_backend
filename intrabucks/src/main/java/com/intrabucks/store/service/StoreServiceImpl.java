68a95951234df2fd798d642657cbbfd32b5690ed
jy <ji9626689@gmail.com>
2024-07-02 21:47:51 +0900

    store 수정중 (오류있음 PR 하지 마세요)

--- intrabucks/src/main/java/com/intrabucks/store/service/StoreServiceImpl.java
+++ intrabucks/src/main/java/com/intrabucks/store/service/StoreServiceImpl.java
@@ -1,14 +1,11 @@
 package com.intrabucks.store.service;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.intrabucks.entity.Manager;
 import com.intrabucks.entity.Store;
-import com.intrabucks.store.data.dto.ManagerRequestStoreDTO;
 import com.intrabucks.store.data.reactdto.Store_StoreDTO;
 import com.intrabucks.store.data.repository.ManagerRepository;
 import com.intrabucks.store.data.repository.StoreRepository;
@@ -31,28 +28,6 @@ public class StoreServiceImpl implements StoreService {
 		return this.storeRepository.findAll();
 	}
   
-	@Override
-	public Store_StoreDTO readStore(Long id) throws NoSuchElementException {
-		// 구현방식: id로 Store를 받아오고 그것을 StoreDTO로 변환
-		// 그리고 리턴
-		Store store = this.storeRepository.findById(id).orElseThrow();
-		Store_StoreDTO storeDto = new Store_StoreDTO();
-		storeDto.newStore(store);
-		return storeDto;
-	}
-	
-
-	@Override
-	public Long regStore(ManagerRequestStoreDTO managerStoreDto) {
-		Manager manager = Manager.builder()
-				.managerId(managerStoreDto.getManagerId())
-				.managerName(managerStoreDto.getManagerName())
-				.managerPassword(managerStoreDto.getManagerPassword())
-				.managerEmail(managerStoreDto.getManagerEmail()).build();
-		this.managerRepository.save(manager);
-		return manager.getManagerId();
-	}
-
 	@Override
 	public Long editStore(Store_StoreDTO storeDto) {
 		// TODO Auto-generated method stub
