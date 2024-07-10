e10b626f26c23ac938838367d9fb3d3ec54b1318
wch <wonchiho9@gamil.com>
2024-07-10 20:39:48 +0900

    feat: 결재 테이블 C,R,U 생성

---
+++ intrabucks/src/main/java/com/intrabucks/approval/controller/Approval2Controller.java
@@ -0,0 +1,41 @@
+package com.intrabucks.approval.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.intrabucks.approval.data.dto.reactdto.Approval_ApprovalDto;
+import com.intrabucks.approval.service.Approval2Service;
+
+@RestController
+@RequestMapping(value = "/api/v1/approval")
+public class Approval2Controller {
+	
+	@Autowired
+	private Approval2Service approval2Service;
+	
+	public Approval2Controller(Approval2Service approval2Service) {
+		this.approval2Service = approval2Service;
+	}
+	
+	@GetMapping("/selectUser/{appDocId}")
+	public ResponseEntity<ArrayList<Approval_ApprovalDto>> selectApprovalUserList(@PathVariable Long appDocId) {
+		ArrayList<Approval_ApprovalDto> approvalList = approval2Service.selectApprovalList(appDocId);
+		return ResponseEntity.ok(approvalList);
+	}
+	
+	@PutMapping("/updateUser")
+	public ResponseEntity<Approval_ApprovalDto> updateApprovalUserList(@RequestBody Approval_ApprovalDto approval_ApprovalDto) {
+		System.out.println("왜죠?");
+		Approval_ApprovalDto approval = approval2Service.updateApproval(approval_ApprovalDto);
+		
+		return ResponseEntity.ok().body(approval);
+	}
+}
--- intrabucks/src/main/java/com/intrabucks/approval/data/repository/ApprovalRepository.java
+++ intrabucks/src/main/java/com/intrabucks/approval/data/repository/ApprovalRepository.java
@@ -1,9 +1,11 @@
 package com.intrabucks.approval.data.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.intrabucks.entity.Approval;
 
 public interface ApprovalRepository extends JpaRepository<Approval, Long> {
-
+	List<Approval> findByDocumentAppDocId(Long appDocId);
 }
