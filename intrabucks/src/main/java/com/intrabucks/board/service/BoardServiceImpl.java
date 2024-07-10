6894c0d59a59efa42e87cc913861205cfe312ada
KCCI104\kcci <kuhj1021@gmail.com>
2024-07-10 15:36:30 +0900

    게시판 업로드 및 다운로드

--- intrabucks/src/main/java/com/intrabucks/board/service/BoardServiceImpl.java
+++ intrabucks/src/main/java/com/intrabucks/board/service/BoardServiceImpl.java
@@ -1,22 +1,29 @@
 package com.intrabucks.board.service;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
-
-import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.intrabucks.board.data.dto.reactdto.dto.Board_BoardDTO;
 import com.intrabucks.board.data.repository.BoardRepository;
 import com.intrabucks.entity.Board;
 
+
+
+
 /**
- * 게시판 관리 기능 관련 ServiceImpl로, 게시판 CRUD 기능 구현
+ * 게시판 관리 기능 관련 ServiceImpl로, 파일 업로드 및 다운로드
  * 
- * @author 김아현
- * @version 1.0 2024-07-04
+ * @author 구은재
+ * @version 2.0
+ * 2024-07-10
  **/
 
 @Service
@@ -103,6 +110,47 @@ public class BoardServiceImpl implements BoardService {
 		return null;
 	}
 
-	// 첨부파일 업로드 코드 작성하기
+	/**파일 업로드&파일 다운로드*/
+	//파일 업로드
+	private static final String UPLOAD_DIR = "src/main/resources/data/Boardfiles/";
+	
+	@Override
+	public Board_BoardDTO saveBoard(Board_BoardDTO boardDto, MultipartFile file) {
+		String fileName = file.getOriginalFilename();
+        try {
+            Files.copy(file.getInputStream(), Paths.get(UPLOAD_DIR + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        Board board = new Board();
+        board.setBoardTitle(boardDto.getBoardTitle());
+        board.setBoardContent(boardDto.getBoardContent());
+        board.setBoardDate(new Date());
+        board.setBoardFile(fileName);
+        board.setEmployee(boardDto.getEmployee());
+        board.setDepartment(boardDto.getDepartment());
+        
+        board = boardRepository.save(board);
+        return new Board_BoardDTO(board.getBoardId(), board.getBoardTitle(), board.getBoardContent(),
+                            board.getEmployee(), board.getBoardDate(), 
+                            board.getBoardFile(), board.getDepartment());
+	}
+
+	//파일 다운로드
+	@Override
+	public byte[] downloadFile(Long boardId) {
+		 Optional<Board> boardOpt = boardRepository.findById(boardId);
+	        if (boardOpt.isPresent()) {
+	            Board board = boardOpt.get();
+	            String fileName = board.getBoardFile();
+	            String filePath = UPLOAD_DIR + fileName;
+	            try {
+	                return Files.readAllBytes(Paths.get(filePath));
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	        return null;
+	    }
 }
